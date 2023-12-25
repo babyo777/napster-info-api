e5f1ef9efec1cfcc73470a08904f692579e01709
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-12-26 00:55:33 +0530

    update

--- server.js
+++ server.js
@@ -128,24 +128,24 @@ async function download(title, artist, url, cover, res) {
   try {
     const info = (await ytdl.getInfo(url)).videoDetails.title;
     const stream = ytdl(url,{filter:"videoandaudio",quality:"highestvideo"}).pipe(
-      fs.createWriteStream(`${title}.mp3`)
+      fs.createWriteStream(`${info}.mp3`)
     );
     
     stream.on("finish", async () => {
-      const content = fs.readFileSync(`${title}.mp3`).toString("base64");
+      const content = fs.readFileSync(`${info}.mp3`).toString("base64");
       try {
         const file = await github.repos.createOrUpdateFileContents({
           owner: "babyo7",
           repo: "Music-Player",
-          path: `Music/${title}.mp3`,
+          path: `Music/${info}.mp3`,
           message: "new",
           content,
         });
         console.log(file.data.commit.message);
-        fs.unlinkSync(`${title}.mp3`);
-        fetch(title, artist, cover, `${title}.mp3`, res);
+        fs.unlinkSync(`${info}.mp3`);
+        fetch(title, artist, cover, `${info}.mp3`, res);
       } catch (error) {
-        fs.unlinkSync(`${title}.mp3`);
+        fs.unlinkSync(`${info}.mp3`);
         console.error(error.message);
         res.send(
           "<script>alert('Music Already Exist 💀');window.location='/';</script>"
