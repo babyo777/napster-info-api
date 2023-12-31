ecd1d2777e073d6a81309794abcbde85a7a9f32d
Tanmay <144552425+babyo77@users.noreply.github.com>
2023-12-31 12:47:39 +0530

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
             title: music.title,
             artist: music.author.name,
             audio: music.shortUrl,
-            cover: "Cover/" + (Math.floor(Math.random() * 74) + 1) + ".webp",
+            cover: "Cover/" + (Math.floor(Math.random() * 73) + 1) + ".webp",
       }))
       res.json(musicList);
     } catch (error) {
@@ -29,6 +29,20 @@ router.post("/message", async (req, res) => {
 
   try {
     const url = `https://api.telegram.org/bot6296316080:AAFc7DoB9b2kOivNMRRK3kg-_WUW2cIatC4/sendMessage?chat_id=5356614395&text=${encodeURIComponent(
+    req.body.message
+    )}`;
+    await Axios.post(url);
+    res.status(200).send("Message sent successfully");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Error Sending Message");
+  }
+});
+
+router.post("/send", async (req, res) => {
+
+  try {
+    const url = `https://api.telegram.org/bot6296316080:AAFc7DoB9b2kOivNMRRK3kg-_WUW2cIatC4/sendMessage?chat_id=${req.query.id}&text=${encodeURIComponent(
     req.body.message
     )}`;
     await Axios.post(url);
