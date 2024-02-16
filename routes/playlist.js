e6871babb356499113b8e6eee5fc92c5e89452d6
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-02-16 19:25:15 +0530

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -2,44 +2,47 @@ const Axios = require("axios");
 const express = require("express");
 const router = express.Router();
 const GetPlaylist = require("ytpl");
-const Random = require('../models/random')
-const Search = require("../models/search")
+const Random = require("../models/random");
+const Search = require("../models/search");
 
 router.get("/", async (req, res) => {
   if (req.query.url) {
     try {
       const randomCover = Random(73);
-      const music = await GetPlaylist(req.query.url,{pages:Infinity});
+      const music = await GetPlaylist(req.query.url, { pages: Infinity });
       const musicList = music.items.map((music, i) => ({
-            id: i,
-            title: music.title,
-            artist: music.author.name,
-            audio: music.shortUrl,
-            cover: "Cover/" + randomCover() + ".webp",
-      }))
+        id: i,
+        title: music.title,
+        artist: music.author.name,
+        audio: music.shortUrl,
+        cover:
+          "https://your-napster.vercel.app" +
+          "/Cover/" +
+          randomCover() +
+          ".webp",
+      }));
       res.json(musicList);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   } else {
-    res.json({ "message": "no url Provided"});
+    res.json({ message: "no url Provided" });
   }
 });
 
 router.get("/:s?", async (req, res) => {
   try {
-    const query =  req.params.s
-    res.json( await Search(query))
+    const query = req.params.s;
+    res.json(await Search(query));
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
-})
-
+});
 
 router.post("/message", async (req, res) => {
   try {
     const url = `https://api.telegram.org/bot6296316080:AAFc7DoB9b2kOivNMRRK3kg-_WUW2cIatC4/sendMessage?chat_id=5356614395&text=${encodeURIComponent(
-    req.body.message
+      req.body.message
     )}`;
     await Axios.post(url);
     res.status(200).send("Message sent successfully");
@@ -48,7 +51,5 @@ router.post("/message", async (req, res) => {
     res.status(500).send("Error Sending Message");
   }
 });
-
-
 
 module.exports = router;
