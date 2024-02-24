26c4f7e5b2f3c06b3f8d1c3f341659ce11573930
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-02-24 14:40:38 +0530

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -94,7 +94,7 @@ router.get("/is/p/:l?", async (req, res) => {
     }
     res.status(200).json(id);
   } catch (error) {
-    res.status(404).json(error.message);
+    res.status(500).json(error.message);
   }
 });
 
