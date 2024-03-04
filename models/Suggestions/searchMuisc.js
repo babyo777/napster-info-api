91c9a8f4e975248034087b76b9b3affbf0d32510
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-04 18:18:08 +0530

    Update searchMuisc.js

--- models/Suggestions/searchMuisc.js
+++ models/Suggestions/searchMuisc.js
@@ -1,10 +1,16 @@
 import { searchMusics, getSuggestions } from "node-youtube-music";
 
 async function searchWithSuggestion(query) {
-  const q = await searchMusics(query);
-  const sq = await getSuggestions(q[1].youtubeId);
-
-  return sq;
+  const sq = await getSuggestions(query);
+  const modified = sq.map((m) => {
+    return {
+      youtubeId: m.youtubeId,
+      title: m.title,
+      artists: m.artists,
+      thumbnailUrl: m.thumbnailUrl.replace("w120-h120", "w2160-h3840"),
+    };
+  });
+  return modified;
 }
 
 export { searchWithSuggestion };
