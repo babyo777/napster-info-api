7714a1e2a1ad03313e54f0a4d1e7186d4259f6e3
babyo77 <144552425+babyo77@users.noreply.github.com>
2024-03-21 11:25:18 +0530

    Update getLrc.js

--- models/getLrc.js
+++ models/getLrc.js
@@ -12,30 +12,7 @@ const searchLRC = async (query) => {
     const q = query.query.replace(/ /gi, "+");
     const url = `https://www.megalobiz.com/search/all?qry=${q}`;
     try {
-      const headers = {
-        Accept: "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.7",
-        "Content-Type": "application/x-www-form-urlencoded",
-        Origin: "https://www.facebook.com",
-        Referer:
-          "https://www.facebook.com/v2.8/plugins/page.php?app_id=178413309233844&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df9bc2c92424a065e3%26domain%3Dwww.megalobiz.com%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fwww.megalobiz.com%252Ff2d62d008f732ce7b%26relation%3Dparent.parent&container_width=288&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2Fmegalobiz&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&width=340px",
-        "Sec-Ch-Ua":
-          '"Chromium";v="122", "Not(A:Brand";v="24", "Brave";v="122"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": '"Windows"',
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Gpc": "1",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "X-Asbd-Id": "129477",
-        "X-Fb-Lsd": "hEgDwwJ3jQ2h1Ha--PWC7t",
-      };
-      const { data } = await axios.get(url, {
-        headers: headers,
-      });
+      const { data } = await axios.get(url);
       const $ = cheerio.load(data);
       const listItems = $(
         ".entity_full_member_info .entity_full_member_name div[class='pro_part mid']"
@@ -79,6 +56,7 @@ async function getLRC(query) {
     query: query,
   });
   const res2 = await getLRc(res[0].link);
+
   return res2;
 }
 
